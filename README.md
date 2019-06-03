44ac85939957ea83e5d3075c7fce5474c52e11f1
ruanpienaar <ruan800@gmail.com>
2019-06-03 22:31:47 +0100

    Some more WIP

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ A holster for [Gun](https://github.com/ninenines/gun)
 $ make
 $ rebar3 shell
 ```
+
+## Example:
+
+First request:
+```Erlang
+{ok, Host, URI, Proto, Port, OptsMap, Timeout, Pid} = holster:req("www.erlang.org", "/").
+```
+
+Consequent requests:
+```Erlang
+holster:req(Host, "/news", Proto, Port, OptsMap, Timeout, Pid).
+holster:req(Host, "/downloads", Proto, Port, OptsMap, Timeout, Pid).
+```
--- src/holster.erl
+++ src/holster.erl
@@ -1,77 +1,100 @@
 -module(holster).
 
 -export([
-    req/1
+    req/1,
+    req/2,
+    req/3,
+    req/4,
+    req/6,
+    req/7
 ]).
 
-req(Url) ->
-    req(Url, http).
+req(Host) ->
+    req(Host, "/").
 
-req(Url, http) ->
-    req(Url, http, 80);
-req(Url, https) ->
-    req(Url, https, 443).
+req(Host, URI) ->
+    req(Host, URI, http).
 
-req(Url, https, Port) when Port /= 443 ->
-    req(Url, https, Port, #{transport => tls}, 1000);
-req(Url, Proto, Port) ->
-    req(Url, Proto, Port, #{}, 1000).
+req(Host, URI, http) ->
+    req(Host, URI, http, 80);
+req(Host, URI, https) ->
+    req(Host, URI, https, 443).
 
-req(Url, Proto, Port, OptsMap, Timeout) ->
-    Pid = spawn_link(fun() -> init(Url, Proto, Port, OptsMap, Timeout) end),
-    Pid ! {self(), Url}.
+req(Host, URI, https, Port) when Port /= 443 ->
+    req(Host, URI, https, Port, #{transport => tls}, 1000);
+req(Host, URI, Proto, Port) ->
+    req(Host, URI, Proto, Port, #{}, 1000).
 
-init(Url, _, Port, OptsMap, Timeout) ->
-    {ok, ConnPid} = gun:open(Url, Port, OptsMap),
+req(Host, URI, Proto, Port, OptsMap, Timeout) ->
+    Pid = spawn_link(fun() ->
+        init(Host, URI, Proto, Port, OptsMap, Timeout)
+    end),
+    {ok, Host, URI, Proto, Port, OptsMap, Timeout, Pid}.
+
+req(Host, URI, Proto, Port, OptsMap, Timeout, Pid) ->
+    Pid ! {client_request, URI},
+    {ok, Host, URI, Proto, Port, OptsMap, Timeout, Pid}.
+
+init(Host, URI, _, Port, OptsMap, Timeout) ->
+    {ok, ConnPid} = gun:open(Host, Port, OptsMap),
     case gun:await_up(ConnPid, Timeout) of
-        {ok, Protocol} ->
-            loop(ConnPid, Timeout);
+        {ok, _Protocol} ->
+            StreamRef = gun:get(ConnPid, URI),
+            loop(ConnPid, Timeout, StreamRef);
         {error, Reason} ->
             {error, Reason}
     end.
 
-loop(ConnPid, Timeout) ->
+loop(ConnPid, Timeout, StreamRef) ->
     receive
-        {ReqPid, Url} ->
-            % Resonse = gun:get(ConnPid, Url),
-            StreamRef = gun:get(ConnPid, Url),
-            Response =
-                receive
-                    {gun_response, ConnPid, StreamRef, fin, Status, Headers} ->
-                        no_data;
-                    {gun_response, ConnPid, StreamRef, nofin, Status, Headers} ->
-                        receive_data(ConnPid, MRef, StreamRef, Timeout);
-                    {'DOWN', MRef, process, ConnPid, Reason} ->
-                        error_logger:error_msg("Connection Down!"),
-                        exit(Reason)
-                after
-                    Timeout ->
-                        exit(timeout)
-                end,
-            ReqPid ! {response, Response},
-            loop(ConnPid);
-        {'DOWN', Mref, process, ConnPid, Reason} ->
+        {client_request, NewUri} ->
+            NewStreamRef = gun:get(ConnPid, NewUri),
+            loop(ConnPid, Timeout, NewStreamRef);
+        {gun_up, _ServerPid, _Proto} ->
+            loop(ConnPid, Timeout, StreamRef);
+        {gun_down, _ServerPid, _Proto, _Reason ,_ , _} ->
+            ok = error_logger:error_msg("Connection Down!"),
+            loop(ConnPid, Timeout, StreamRef);
+        {gun_response, ConnPid, StreamRef, fin, Status, Headers} ->
+            error_logger:info_msg(
+                "fin - Response Headers ~p\nStatus ~p",
+                [Headers, Status]
+            ),
+            no_data;
+        {gun_response, ConnPid, StreamRef, nofin, Status, Headers} ->
+            error_logger:info_msg(
+                "nofin - Response Headers ~p\nStatus ~p",
+                [Headers, Status]
+            ),
+            receive_data(ConnPid, StreamRef, Timeout),
+            loop(ConnPid, Timeout, StreamRef);
+        {'DOWN', _MRef, process, ConnPid, Reason} ->
+            error_logger:error_msg("Connection Down!"),
+            exit(Reason);
+        {'DOWN', _Mref, process, ConnPid, Reason} ->
             ok = error_logger:error_msg("Connection Down!"),
             exit(Reason);
         close ->
+            ok = error_logger:error_msg("Connection Closed!"),
             ok = gun:cancel(ConnPid, StreamRef),
-            ok = gun:close(ConnPid);
-        Msg ->
-            io:format("Received Msg ~p\n\n", [Msg]),
-            loop(ConnPid)
+            ok = gun:close(ConnPid),
+            exit(close);
+        UnhandledMsg ->
+            io:format("Received UnhandledMsg ~p\n\n", [UnhandledMsg]),
+            loop(ConnPid, Timeout, StreamRef)
 end.
 
-receive_data(ConnPid, MRef, StreamRef, Timeout) ->
+receive_data(ConnPid, StreamRef, Timeout) ->
     receive
         {gun_data, ConnPid, StreamRef, nofin, Data} ->
             io:format("~s~n", [Data]),
-            receive_data(ConnPid, MRef, StreamRef);
+            receive_data(ConnPid, StreamRef, Timeout);
         {gun_data, ConnPid, StreamRef, fin, Data} ->
             io:format("~s~n", [Data]);
-        {'DOWN', MRef, process, ConnPid, Reason} ->
-            ok = error_logger:error_msg("Oops!"),
+        {'DOWN', _MRef, process, ConnPid, Reason} ->
+            ok = error_logger:error_msg("receive data {}"),
             exit(Reason)
     after
-        1000 ->
+        Timeout ->
             exit(timeout)
     end.
